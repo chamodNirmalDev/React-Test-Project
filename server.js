1fe8c2ee50bb12490c155a7c65b56557bffbbbf7
chamod nirmal <ChamodNirmalDev@gmail.com>
2026-09-05 16:54:00 +0530

    Creat server and modified server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+const app = require('./app');
+const port = 3001;
+const host = '127.0.0.1';
+
+const server = app.listen(port, host, () => {
+    console.log(`Node Server is listening to ${server.address().port} - server.js:6`)
+});
